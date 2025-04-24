cf3011719aece0c4115c82a19b6913a9a08f19ba
tuan2004a <bi03112004@gmail.com>
2025-04-24 19:10:35 +0700

    style: card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
     if(!selecLightCone)return null;
 
     return (
-        <div className='flex items-center space-x-10 '>
+        <div className='flex items-center '>
             <div  className='relative w-[307px] h-[430px] rotate-5'>
                 <div className=' absolute z-10 w-[307px] h-[430px] bg-white/10 top-0 left-0 after:absolute after:rounded after:w-[87%] after:h-[90%] after:border-2 after:border-amber-100 after:top-1/2 after:left-1/2 after:translate-[-50%]' style={{clipPath: "polygon(5% 0, 95% 0, 100% 4%, 100% 96%, 95% 100%, 5% 100%, 0 96%, 0 4%)",}}></div>
                 <div className=' absolute z-20 w-[307px] -translate-x-5.5 -translate-y-5 '>
